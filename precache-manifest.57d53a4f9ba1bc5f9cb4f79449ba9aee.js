7e6c590681e8e49eba9e07cc7b1268b51977077a
wuxz <wuxz@liandisys.com.cn>
2018-10-31 16:47:13 +0900

    add android notification

---
+++ precache-manifest.57d53a4f9ba1bc5f9cb4f79449ba9aee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "209609b77614694301d5",
+    "url": "/js/chunk-vendors.5586e76f.js"
+  },
+  {
+    "revision": "6900c4a87ba672eb6da5",
+    "url": "/js/articleDetail.d8da492f.js"
+  },
+  {
+    "revision": "d3cc06bc00c4b9ed5f85",
+    "url": "/js/app.8bd2c16d.js"
+  },
+  {
+    "revision": "ad8f1957d150b82c9c9d14aca75fb827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9196cd70463257bf2e49b6d3bf5e2ec7",
+    "url": "/css/common.css"
+  },
+  {
+    "revision": "6900c4a87ba672eb6da5",
+    "url": "/css/articleDetail.02a816bf.css"
+  },
+  {
+    "revision": "d3cc06bc00c4b9ed5f85",
+    "url": "/css/app.6b530fc3.css"
+  }
+];
