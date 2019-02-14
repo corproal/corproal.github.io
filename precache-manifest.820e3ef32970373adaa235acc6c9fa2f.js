a16efae6b428204cbfb953d823b96a894d0095f5
wuxz <wuxz@liandisys.com.cn>
2019-02-14 11:20:43 +0900

    test

--- precache-manifest.820e3ef32970373adaa235acc6c9fa2f.js
+++ precache-manifest.820e3ef32970373adaa235acc6c9fa2f.js
@@ -4,11 +4,11 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.62e04578.js"
   },
   {
-    "revision": "a60c737f2089def287c8",
-    "url": "/js/app.860e2198.js"
+    "revision": "be1408accdd36932adf3",
+    "url": "/js/app.c27ee4a9.js"
   },
   {
-    "revision": "aacbb08c19b8a503fa0965791900ad55",
+    "revision": "1098b3762f506e1e96a578913bf85884",
     "url": "/index.html"
   },
   {
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/css/common.css"
   },
   {
-    "revision": "a60c737f2089def287c8",
+    "revision": "be1408accdd36932adf3",
     "url": "/css/app.a248f7b6.css"
   }
 ];
